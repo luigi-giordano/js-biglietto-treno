11a07eeaa6584aba108d03aa59c86deed073a144
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-13 17:28:11 +0100

    modify code and fix console.log

--- script.js
+++ script.js
@@ -15,22 +15,18 @@ Questo richiederà un minimo di ricerca.*/
 const priceTicketKm = 0.21;
 const discountUnderAge = 20;
 const discountOverAge = 40;
+const priceTicket = 0.21 * totalKm;
 
 let totalKm = parseInt(prompt('Quanti km devi percorrere?'));
 let agePassenger = parseInt(prompt('Quanti anni hai?'));
 
-const priceTicket = 0.21 * totalKm;
-const priceUnderAge = priceTicket * (1 - discountUnderAge / 100);
-const priceOverAge = priceTicket * (1 - discountOverAge / 100);
-
 let finalPrice
 
 if (agePassenger < 18){
+  const priceUnderAge = priceTicket * (1 - discountUnderAge / 100);
   finalPrice = priceUnderAge
-  console.log('Il prezzo finale sarà', finalPrice.toFixed(2));
 } else if (agePassenger > 65){
+  const priceOverAge = priceTicket * (1 - discountOverAge / 100);
   finalPrice = priceOverAge
-  console.log('Il prezzo finale sarà', finalPrice.toFixed(2));
-} else {
-  console.log('Il prezzo finale sarà', priceTicket.toFixed(2));
 }
+console.log('Il prezzo finale sarà', finalPrice.toFixed(2));
